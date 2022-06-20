ff2222d87371a69f5e08efb9cc9d82a37f965b19
Ivan <46783751+AlgorithmLX@users.noreply.github.com>
2022-06-20 13:18:26 +0700

    update

--- src/main/java/liquid/network/Direction.java
+++ src/main/java/liquid/network/Direction.java
@@ -2,6 +2,8 @@ package liquid.network;
 
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.DistExecutor.*;
 
 public interface Direction {
     void init();
@@ -11,4 +13,16 @@ public interface Direction {
     Level clientLevel();
 
     Player clientPlayer();
+
+    static <T extends Direction> Direction of(SafeSupplier<T> client, SafeSupplier<T> server) {
+        return DistExecutor.safeRunForDist(()-> client, ()-> server);
+    }
+
+    static <T extends Direction> Direction clientOnly(SafeSupplier<T> client) {
+        return Direction.of(client, null);
+    }
+
+    static <T extends Direction> Direction serverOnly(SafeSupplier<T> server) {
+        return Direction.of(null, server);
+    }
 }
